b6e057f0209982c015ca150adcb7dfc2a33a4e86
Melanie Cisler <65982774+melaniecisler@users.noreply.github.com>
2020-12-17 12:20:43 -0600

    Update Reserve.js

--- src/components/pages/Reserve.js
+++ src/components/pages/Reserve.js
@@ -1,23 +1,83 @@
-import React from "react";
-import { Link, Route } from "react-router-dom";
-import Booked from "./Booked";
+import React, { Component } from 'react';
 
-function Reserve(props) {
-  return (
-    <div>
-      <h1>Reserve A Chat</h1>
-      <p>
-       Topic<br></br>
-       Date<br></br>
-       Time<br></br>
-      </p>
-      <Link to={`${props.match.url}/booked`} role="button" className="btn btn-link">
-        Book Now
-      </Link>{" "}
+import Form from 'react-input';
+import './Reserve.css';
+
+const INITIAL_STATE = {
+  email: null,
+  name: null,
+  topic: null,
+  date: null,
+  time: null
+  
+};
+
+class Reserve extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = INITIAL_STATE;
+  }
+
+  render() {
+    return (
       
-      <Route exact path={`${props.match.url}/booked`} component={Booked} />
-    </div>
-  );
+      <div>
+        <div>Email: {this.state.email}</div>
+        <div>Name: {this.state.name}</div>
+        <div>Topic: {this.state.topic}</div>
+        <div>Date: {this.state.date}</div>
+        <div>Time: {this.state.time}</div>
+        <hr />
+        <Form
+          fields={[
+            {
+              name: 'Email',
+              key: 'email',
+              type: 'email',
+              placeholder: 'Enter an email'
+            },
+            {
+              name: 'Name',
+              key: 'name',
+              type: 'text',
+              placeholder: 'Enter a name'
+            },
+            {
+              name: 'Topic',
+              key: 'topic',
+              type: 'text',
+              placeholder: 'Enter a topic'
+            },
+            {
+              name: 'Date',
+              key: 'date',
+              type: 'text',
+              placeholder: 'Enter a date'
+            },
+            {
+              name: 'Time',
+              key: 'time',
+              type: 'text',
+              placeholder: 'Enter a time'
+            }
+          ]}
+          labels={false}
+          onSubmit={({ email, name, topic, date, time }) => this.setState({
+            email,
+            name,
+            topic,
+            date,
+            time
+
+          })}
+          
+        />
+        
+      </div>
+    );
+  }
 }
+
 
 export default Reserve;
